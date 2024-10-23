589cf874cff88435026a74d8db5036e9973fb6c2
lxs <lxs@lxs>
2024-10-23 15:54:27 +0800

    文章详情锚点问题

--- src/components/blog/PostsHome.vue
+++ src/components/blog/PostsHome.vue
@@ -1,7 +1,7 @@
 <template>
   <!--导航-->
   <NavBlog />
-  <div class="container-fixed" :style="{ height: contentHeight + 'px' }">
+  <div class="container-fixed">
     <div id="markdown-container" class="col-content" ></div>
   </div>
   <!--底部-->
@@ -13,36 +13,15 @@ import 'cherry-markdown/dist/cherry-markdown.css';
 import Cherry from 'cherry-markdown';
 import NavBlog from "@/components/blog/module/NavBlog.vue";
 import FooterHome from "@/components/module/FooterHome.vue";
-import { onMounted, onUnmounted, ref } from 'vue';
 
 export default {
   components: {
-    FooterHome,
-    NavBlog
-  },
-  setup() {
-    const contentHeight = ref(0);
-    const handleResize = () => {
-      const container = document.querySelector('.container').offsetHeight;
-      const footer = document.querySelector('.footer').offsetHeight;
-      contentHeight.value = window.innerHeight - container - footer - 40;
-    };
-    onMounted(() => {
-      handleResize(); // 初始化高度
-      window.addEventListener('resize', handleResize);
-    });
-
-    onUnmounted(() => {
-      window.removeEventListener('resize', handleResize);
-    });
-    return {
-      contentHeight
-    };
+    FooterHome, NavBlog
   },
   mounted() {
     new Cherry({
       id: 'markdown-container',
-      value: '## hello world\nhello world\nhello world\nhello world\nhello world\nhello world\nhello world\nhello world\n' +
+      value: '## hello #world\nhello world\nhello world\nhello world\nhello world\nhello world\nhello world\nhello world\n' +
         '### 123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n123\n' +
         '#### 122344',
       engine: {
@@ -61,11 +40,27 @@ export default {
           updateLocationHash: false, // 要不要更新URL的hash
           defaultModel: 'full', // pure: 精简模式/缩略模式，只有一排小点； full: 完整模式，会展示所有标题
           showAutoNumber: false, // 是否显示自增序号
-          position: 'absolute', // 悬浮目录的悬浮方式。当滚动条在cherry内部时，用absolute；当滚动条在cherry外部时，用fixed
+          position: 'fixed', // 悬浮目录的悬浮方式。当滚动条在cherry内部时，用absolute；当滚动条在cherry外部时，用fixed
           cssText: '', // 自定义样式
         },
       }
-    });
+    })
+    document.addEventListener('click', function(event) {
+      if (event.target.tagName === 'A' && event.target.getAttribute('data-id')) {
+        event.preventDefault();
+        const targetId = event.target.getAttribute('data-id').replace(/^#/, ""); // 获取锚点的目标ID
+        const targetElement = document.querySelector(`[id="${targetId}"]`); // 查找目标元素
+        if (targetElement) {
+          // 计算目标元素的顶部位置
+          const targetPosition = targetElement.getBoundingClientRect().top + window.scrollY - 90; // 减去90px
+          // 平滑滚动到目标位置
+          window.scrollTo({
+            top: targetPosition,
+            behavior: 'smooth' // 平滑滚动
+          });
+        }
+      }
+    })
   }
 }
 </script>
@@ -76,7 +71,7 @@ export default {
   width: 100%;
   margin-left: auto;
   margin-right: auto;
-  margin-top: 70px;
+  margin-top: 60px;
 }
 
 @media screen and (max-width: 1366px) {
